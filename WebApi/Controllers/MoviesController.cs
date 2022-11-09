3c16171079f62bf40aa8914b1124c37bf95ff913
Rafał Polar <polar.rafal@gmail.com>
2022-11-09 10:00:28 +0100

    refactor

---
+++ WebApi/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.DAL;
+using WebApi.Models.Entities;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/movies")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Movie>> Get(int id)
+        {
+            var movie = await _context.Movies.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(movie);
+        }
+    }
+}
